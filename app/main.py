11be3de2c8b754894be7a62a3c3d9dae15bd4c46
K4VOOM <andrejseret@gamil.com>
2026-09-24 22:31:19 +0300

    add quiz answer handler with step progression, update README

--- app/main.py
+++ app/main.py
@@ -11,11 +11,13 @@ from app.db.session import async_session
 from app.middlewares.db import DbSessionMiddleware
 
 from app.handlers.start import router as start_router
+from app.handlers.quiz import router as quiz_router
 
 TOKEN = settings.bot_token
 
 dp = Dispatcher()
 dp.include_router(start_router)
+dp.include_router(quiz_router)
 
 dp.update.outer_middleware(DbSessionMiddleware(session_pool=async_session))
 
